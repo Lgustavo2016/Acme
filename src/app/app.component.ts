8305d71908354b1e5780b8ae65815b7006b13365
Luiz Gustavo <lgustavo728@gmail.com>
2018-07-30 22:14:08 -0300

    UPDATE | Added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core'
   selector: 'pm-root',
   //Template is the HTML content that will be displayed between the custom directive defined above
   template: `
-<div><h1>{{pageTitle}}</h1>
-<div><pm-products></pm-products></div>
+<nav class='navbar navbar-expand navbar-light bg-light'>
+<a class='navbar-brand'>{{pageTitle}}</a>
+<ul class='nav nav-pills'>
+<li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+<li><a class='nav-link' [routerLink]="['/products']">Product</a></li>
+</ul>
+</nav>
+<div class='container'>
+<router-outlet></router-outlet>
 </div>
 `
 })
